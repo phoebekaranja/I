e882d378631e6b725e61e6b665200b26363ce80b
phoebekaranja <phoebemurish@gmail.com>
2019-05-18 21:59:59 +0300

    created the first welcome function

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def welcome(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.user = current_user
+            profile.save()
+        return redirect('news-Profile')
+    else:
+        form = NewProfileForm()
+    return render(request,'welcome.html',{'form':form})
