c2041914da3a8e978368af5ed1ff9b96b01ad453
phoebekaranja <phoebemurish@gmail.com>
2019-05-19 16:37:00 +0300

    configured database

--- Images/settings.py
+++ Images/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'Images.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'I',
+        'USER': 'pheoebe',
+    'PASSWORD':'2019',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
