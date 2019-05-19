aff2226e3278c471c15752a33553b761de74baec
phoebekaranja <phoebemurish@gmail.com>
2019-05-19 16:52:09 +0300

    modified admin file

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile,Image,Comment
+
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Comment',)
+
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comment)
