c6426156e12c19d09a48ea9b274b064bdfab226d
phoebekaranja <phoebemurish@gmail.com>
2019-05-20 16:23:24 +0300

    enable gzip

--- Images/settings.py
+++ Images/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
